13c8cb4bf005bfc83db651c4ecfc10532fe5b6f1
Mehr <you@example.com>
2019-08-01 18:02:00 +0430

    Evaluation part added

--- Variables.py
+++ Variables.py
@@ -9,3 +9,8 @@ print("Float example: " + str(f))
 # String
 S = 'hello world!'
 print("String example: " + S)
+
+# Evaluation
+# this use for calculation app for example that get string as input and solve that string
+E = eval('2 + 2')
+print("Evaluation ('2 + 2') example: " + str(E))
