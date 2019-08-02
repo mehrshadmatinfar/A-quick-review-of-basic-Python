e3de5a24eac6018b04d5282f1200dc63c3583deb
Mehr <you@example.com>
2019-08-02 18:59:26 +0430

    Division part added

--- Arithmetic_Operators.py
+++ Arithmetic_Operators.py
@@ -9,3 +9,10 @@ print("8 - 2 = " + str(a))
 # Multiplication
 a = 3 * 4
 print("3 * 4 = " + str(a))
+
+# Division
+a = 4 / 2
+b = 1 / 3
+print("4 / 2 = " + str(a))
+print("int(4 / 2) = " + str(int(a)))
+print("1 / 3 = " + str(b))
