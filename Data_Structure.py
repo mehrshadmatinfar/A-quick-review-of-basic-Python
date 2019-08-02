9355070d95ff72d75de6ecf2dfcb2502cef8005e
Mehr <you@example.com>
2019-08-02 22:00:55 +0430

    array & List part created

--- Data_Structure.py
+++ Data_Structure.py
@@ -7,6 +7,13 @@ print(a[:5])
 print('Length = ' + str(len(a)))
 
 # array & List
+List = [[1, 2, 3],
+        [5, 8, 3],
+        [9, 6, 4],
+        'This is not Matrix']
+print(List)
+print(List[1][2])
+print(List[3])
 
 # Tuple
 
