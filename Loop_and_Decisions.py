cd63c41324fba871eb8212c8e77fbae97de6bfdb
Mehr <you@example.com>
2019-08-03 00:38:03 +0430

    While part added

--- Loop_and_Decisions.py
+++ Loop_and_Decisions.py
@@ -1,7 +1,7 @@
 # IF
 a = 1
 b = 2
-c = 6
+c = 7
 
 if a == b:
     print("a == b")
@@ -10,3 +10,11 @@ elif a == c and a != c:
 else:
     print("a != b")
 print("I'm not in if 'HA HA HA HA...'")
+
+# While
+while a <= c:
+    print(a)
+    a += 1
+else:
+    print("stop")
+    a = 1
