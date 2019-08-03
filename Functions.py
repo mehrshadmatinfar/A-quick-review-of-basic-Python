206a40d1b2e0e2c5e0d04dee537f13e95f88e6fa
Mehr <you@example.com>
2019-08-03 18:59:08 +0430

    class part added

--- Functions.py
+++ Functions.py
@@ -1,3 +1,20 @@
+class program:
+    name = ""
+    ID = 0
+
+
+a = program()
+a.name = "Hello World!"
+a.ID = 19
+
+b = program()
+b.name = "bye world!"
+b.ID = 33
+b.add_To_Class = 59
+
+print(b.add_To_Class)
+
+
 def even(number):
     if number % 2 == 0:
         return True
