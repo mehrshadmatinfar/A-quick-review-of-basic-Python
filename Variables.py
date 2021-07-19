c4b100d101930d460168d3416de3da7c78d2e3c6
Mehrshad Matinfar <mehr.matinfar@gmail.com>
2021-07-20 00:07:31 +0430

    casting added

--- Variables.py
+++ Variables.py
@@ -22,3 +22,14 @@ print("String example: " + S)
 # this use for calculation app for example that get string as input and solve that string
 E = eval('2 + 2')
 print("Evaluation ('2 + 2') example: " + str(E))
+
+# Python Casting
+print('--- Casting ---')
+print('I as integer: ' + str(I))
+print('I as float: ' + str(float(I)))
+print('I as complex: ' + str(complex(I)))
+
+print('F as float: ' + str(F))
+print('F as integer: ' + str(int(F)))
+print('F as complex: ' + str(complex(F)))
+
