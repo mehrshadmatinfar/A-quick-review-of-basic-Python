07874ab20609d52aa340f72113d87f7a6b1883a1
Mehrshad Matinfar <mehr.matinfar@gmail.com>
2021-07-19 21:24:30 +0430

    type added

--- Logical_Operators.py
+++ Logical_Operators.py
@@ -4,18 +4,22 @@ c = 4
 d = 1
 
 # and
+print('------- and -------')
 C = a == b and a == c
 print("a == b and a == c --> " + str(C))
 C = a == b and a <= c
 print("a == b and a <= c --> " + str(C))
+print(type(C))
 
 # or
+print('------- or --------')
 C = a == b or a == c
 print("a == b or a == c --> " + str(C))
 C = a == b or a <= c
 print("a == b or a <= c --> " + str(C))
 
 # not
+print('------- not -------')
 C = not(a == b and a == c)
 print("not(a == b and a == c) --> " + str(C))
 C = not(a == b and a <= c)
